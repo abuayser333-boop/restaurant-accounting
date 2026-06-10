16a6859cdc13e45141213dd23257ccc4c3237ff9
abuayser333-boop <abuayser333@gmail.com>
2026-06-11 00:57:44 +0300

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -3,14 +3,18 @@ modules/__init__.py
 وحدات نظام محاسبة المطاعم
 """
 
-from .invoices import InvoicesManager
-from .expenses import ExpensesManager
-from .inventory import InventoryManager
-from .reports import ReportsManager
+from .invoices     import InvoicesManager
+from .expenses     import ExpensesManager
+from .inventory    import InventoryManager
+from .reports      import ReportsManager
+from .breakeven    import BreakEvenManager
+from .depreciation import DepreciationManager
 
 __all__ = [
     "InvoicesManager",
     "ExpensesManager",
     "InventoryManager",
     "ReportsManager",
+    "BreakEvenManager",
+    "DepreciationManager",
 ]
